72d521f449a4b6bb5e341f86a498eb88cf723e39
Zeynep Sena Tatlı <zeynepsenatatli@gmail.com>
2022-11-22 22:41:05 +0300

    create club model

--- tauclubs/clubs/models.py
+++ tauclubs/clubs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from accounts import Manager
+from accounts import Member
 
 # Create your models here.
+
+class Club(models.Model):
+    name = models.CharField()
+    manager = models.OneToOneField(Manager, on_delete=models.SET_NULL, blank=True, null=True) 
+    members = models.ManyToManyField(Member, blank=True, null=True)
+    responsibleLecturer = models.TextField()
+    clubMail = models.EmailField()
