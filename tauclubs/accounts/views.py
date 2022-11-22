269c427e072bf74d143f14ef9b3e39bfba3b3fef
EgeCagdas-Aladag <egecagdasaladag01@hotmail.com>
2022-11-22 10:27:12 +0300

    add in users viewset and setup apps

--- tauclubs/accounts/views.py
+++ tauclubs/accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import UserSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    A simple viewset for viewing and editing accounts.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer # Serializer not created yet
+    permission_classes = []
